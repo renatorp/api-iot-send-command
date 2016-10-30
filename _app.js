58785c04bb8f9c3401f60b3855c61850132f9cb2
renatorp <renatorp@github.com>
2016-10-30 09:30:51 -0200

    Limpando projeto

--- _app.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var app = express();
- 
-var routes = require("./routes/routes.js")(app);
- 
-var server = app.listen(3000, function () {
-    console.log("Listening on port %s...", server.address().port);
-});
